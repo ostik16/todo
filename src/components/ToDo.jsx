dfe47c3a6f0120d02cf19e8a3dbed2cf74eeb501
ostik16 <47577504+ostik16@users.noreply.github.com>
2021-02-22 21:56:53 +0100

    Add files via upload

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -35,10 +35,10 @@ const ToDo = ({text, created, todos, setTodos, name, position}) => {
     const alarm = () => {
         setTodos(todos.map(item => {
             if(item.created === created){
-                let timeout = prompt("In how many seconds would you like to get an alert? (if empty, notification will be canceled)");
-                if(timeout == "" || timeout == null) return item
+                let time = prompt("Set time when to show notification.\nDefault value is current time.\n(leave empty to cancel)", new Date().toUTCString());
+                if(time == "" || time == null) return item
                 return{
-                    ...item, notificationTime: new Date().getTime() + timeout * 1000
+                    ...item, notificationTime: new Date(time).getTime()
                 }
             }
             return item
@@ -88,7 +88,7 @@ const ToDo = ({text, created, todos, setTodos, name, position}) => {
                         <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                     </svg>
                 </button>
-                <button onClick={alarm}  className="alarm">
+                <button onClick={alarm} className="alarm">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-alarm-fill" viewBox="0 0 16 16">
                         <path d="M6 .5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1H9v1.07a7.001 7.001 0 0 1 3.274 12.474l.601.602a.5.5 0 0 1-.707.708l-.746-.746A6.97 6.97 0 0 1 8 16a6.97 6.97 0 0 1-3.422-.892l-.746.746a.5.5 0 0 1-.707-.708l.602-.602A7.001 7.001 0 0 1 7 2.07V1h-.5A.5.5 0 0 1 6 .5zm2.5 5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5zM.86 5.387A2.5 2.5 0 1 1 4.387 1.86 8.035 8.035 0 0 0 .86 5.387zM11.613 1.86a2.5 2.5 0 1 1 3.527 3.527 8.035 8.035 0 0 0-3.527-3.527z"/>
                     </svg>
